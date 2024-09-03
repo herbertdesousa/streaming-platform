312845f3837f9ed26a3628457a0fc4fcae1fb41d
herbertdesousa <sousaherbert138@gmail.com>
2024-09-03 20:58:59 -0300

    feat(Home): card implementation

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,6 +1,7 @@
 import "./Home.css";
 
 import { Nav } from "../components/Nav";
+import { HomeCard } from "../components/HomeCard";
 
 import HomeHeroPNG from "../assets/home-hero.png";
 
@@ -27,6 +28,33 @@ export function Home() {
 
         <img src={HomeHeroPNG} />
       </main>
+
+      <section>
+        <HomeCard
+          num="01"
+          title="Youtube"
+          bgColor="red"
+          description="Produza conteúdo cativante e conquiste seu público com vídeos de alta qualidade no maior palco digital do mundo."
+        />
+        <HomeCard
+          num="02"
+          title="Tiktok"
+          bgColor="yellow"
+          description="Faça sua criatividade brilhar em vídeos curtos e envolventes que vão cativar a comunidade global do TikTok."
+        />
+        <HomeCard
+          num="03"
+          title="Facebook"
+          bgColor="green"
+          description="Conecte-se com sua audiência de forma autêntica e impactante através de vídeos que se destacam no feed do Facebook."
+        />
+        <HomeCard
+          num="04"
+          title="Instagram"
+          bgColor="blue"
+          description="Compartilhe suas histórias de maneira única e conquiste milhões de likes no Instagram utilizando Stories e Reels."
+        />
+      </section>
     </div>
   );
 }
