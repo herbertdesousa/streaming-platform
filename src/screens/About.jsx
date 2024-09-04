d4be39ae4ea793c32612e996153f3500958266cc
herbertdesousa <sousaherbert138@gmail.com>
2024-09-03 21:46:42 -0300

    feat(About): main

--- src/screens/About.jsx
+++ src/screens/About.jsx
@@ -2,10 +2,34 @@ import "./About.css";
 
 import { Nav } from "../components/Nav";
 
+import AboutPhone1PNG from "../assets/about-phone-1.png";
+import AboutPhone2PNG from "../assets/about-phone-2.png";
+
 export function About() {
   return (
     <div className="wrapper">
       <Nav />
+
+      <main>
+        <div className="left">
+          <div>
+            <h1>Bem-vindo à revolução dos vídeos!</h1>
+
+            <h2>
+              Somos uma startup com o objetivo de facilitar a produção de
+              conteúdo em larga escala com qualidade profissional, capaz de
+              atingir uma audiência de milhões de seguidores.
+            </h2>
+          </div>
+
+          <button type="button" className="btn">
+            Baixe o app
+          </button>
+        </div>
+
+        <img src={AboutPhone1PNG} />
+        <img src={AboutPhone2PNG} />
+      </main>
     </div>
   );
 }
