39a48acafec6d0a23b042ee150428cc9b7d7b76d
herbertdesousa <sousaherbert138@gmail.com>
2024-09-03 21:37:22 -0300

    feat(App): create browser router

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
 import { Home } from "./screens/Home";
+import { About } from "./screens/About";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Home />,
+  },
+  {
+    path: "/sobre",
+    element: <About />,
   },
 ]);
 
