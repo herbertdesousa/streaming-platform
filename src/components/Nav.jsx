4cf319946c1e2cfd5b5b895add82312e1c603b51
herbertdesousa <sousaherbert138@gmail.com>
2024-09-03 21:37:53 -0300

    feat(Nav): route indicator

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,11 +1,24 @@
+import { useLocation, Link } from "react-router-dom";
 import "./Nav.css";
 
 export function Nav() {
+  const { pathname } = useLocation();
+
+  const isHome = pathname === "/";
+  const isAbout = pathname === "/sobre";
+  const isContact = pathname === "/contato";
+
   return (
     <nav>
-      <span>Home</span>
-      <span>Sobre</span>
-      <span>Contato</span>
+      <span className={isHome ? "active" : ""}>
+        <Link to="/">Home</Link>
+      </span>
+      <span className={isAbout ? "active" : ""}>
+        <Link to="/sobre">Sobre</Link>
+      </span>
+      <span className={isContact ? "active" : ""}>
+        <Link to="/contato">Contato</Link>
+      </span>
     </nav>
   );
 }
